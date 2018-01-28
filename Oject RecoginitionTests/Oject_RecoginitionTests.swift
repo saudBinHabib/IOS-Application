9bbf72f07906b24c8c33e62450b477ff27748965
User <user@minimalist.local>
2018-01-28 23:25:52 +0100

    Initial Commit

---
+++ Oject RecoginitionTests/Oject_RecoginitionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Oject_RecoginitionTests.swift
+//  Oject RecoginitionTests
+//
+//  Created by User on 28.01.18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import XCTest
+@testable import Oject_Recoginition
+
+class Oject_RecoginitionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
